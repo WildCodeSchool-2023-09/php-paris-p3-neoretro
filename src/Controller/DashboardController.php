bca5499ad26f3e95d111050d990431f884e9b311
Nicolas Alibert <122890983+nicolasalibert@users.noreply.github.com>
2023-12-22 17:17:20 +0100

    Update DashboardController.php

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -14,18 +14,31 @@ use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
 
 
-#[Route('/dashboard', 'dashboard_')]
+#[Route('/', 'dashboard_')]
 class DashboardController extends AbstractController
 {
-    #[Route('/user', name: 'user')]
-    public function user(): Response
+    #[Route('/', name: 'guest')]
+    public function guest(): Response
     {
-        return $this->render('dashboard/user.html.twig');
+        return $this->render('dashboard/guest.html.twig', [
+            'pageTitle' => 'NeoRetro',
+        ]);
     }
 
-    #[Route('/admin', name: 'admin')]
+    #[Route('user/', name: 'user')]
+    public function user(): Response
+    {
+        return $this->render('dashboard/user.html.twig', [
+            'pageTitle' => 'Dashboard',
+        ]);
+    }
+
+    #[Route('admin/', name: 'admin')]
     public function admin(): Response
     {
-        return $this->render('dashboard/admin.html.twig');
+        return $this->render('dashboard/admin.html.twig', [
+            'pageTitle' => 'Dashboard',
+            'admin' => true
+        ]);
     }
 }

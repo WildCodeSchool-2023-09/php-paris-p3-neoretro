d3da2935f8b1dddb12245a3059547532a08dd26a
WISSAL KERKOUR <wsskekrour@gmail.com>
2024-01-05 11:01:13 +0100

    [UPDATE] comments

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,45 +6,52 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Faker\Factory;
 
 class UserFixtures extends Fixture
 {
     private UserPasswordHasherInterface $userPasswordHasher;
-    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    private $faker;
+
+    public function __construct (UserPasswordHasherInterface $userPasswordHasher)
     {
         $this->userPasswordHasher = $userPasswordHasher;
+        $this->faker = Factory::create();
     }
+
     public function load(ObjectManager $manager): void
     {
+        // Création d'un utilisateur normal
         $user = new User();
-        $user->setUsername('wissal');
-        $user->setFirstname('wissallu');
-        $user->setLastname('kerkour');
-        $user->setEmail('wsskerkour@gmail.com');
-        $user->setPhonenumber('0763571012');
+        $user->setUsername($this->faker->userName);
+        $user->setFirstname($this->faker->firstName);
+        $user->setLastname($this->faker->lastName);
+        $user->setEmail($this->faker->email);
+        $user->setPhonenumber($this->faker->phoneNumber);
         $user->setRoles([]);
-        $user->setToken(3);
-        $user->setZipcode('78190');
-        $user->setAdress('danslsepthuit');
-        $user->setCity('city');
-        $user->setExperience(3);
-        $user->setPassword($this->userPasswordHasher->hashPassword($user, '1234'));
+        $user->setToken($this->faker->randomNumber(2));
+        $user->setZipcode($this->faker->postcode);
+        $user->setAdress($this->faker->streetAddress);
+        $user->setCity($this->faker->city);
+        $user->setExperience($this->faker->randomNumber(1));
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, 'password123'));
         $manager->persist($user);
 
+        // Création d'un utilisateur admin
         $admin = new User();
-        $admin->setUsername('eli');
-        $admin->setFirstname('elib');
-        $admin->setLastname('bandier');
-        $admin->setEmail('test@gmail.com');
-        $admin->setPhonenumber('0101030405');
+        $admin->setUsername($this->faker->userName);
+        $admin->setFirstname($this->faker->firstName);
+        $admin->setLastname($this->faker->lastName);
+        $admin->setEmail($this->faker->email);
+        $admin->setPhonenumber($this->faker->phoneNumber);
         $admin->setRoles(['ROLE_ADMIN']);
-        $admin->setToken(7);
-        $admin->setZipcode('76190');
-        $admin->setAdress('danslsrrthuit');
-        $admin->setCity('cuty');
-        $admin->setExperience(6);
-        $admin->setPassword($this->userPasswordHasher->hashPassword($user, '5678'));
-        $manager->persist($user);
+        $admin->setToken($this->faker->randomNumber(2));
+        $admin->setZipcode($this->faker->postcode);
+        $admin->setAdress($this->faker->streetAddress);
+        $admin->setCity($this->faker->city);
+        $admin->setExperience($this->faker->randomNumber(1));
+        $admin->setPassword($this->userPasswordHasher->hashPassword($admin, 'admin123'));
+        $manager->persist($admin);
 
         $manager->flush();
     }

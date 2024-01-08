9b57a915cf00943c0ef935d9ff36493ae6620354
WISSAL <ECOLE@simplonco-Latitude-E6420.localdomain>
2024-01-08 17:42:37 +0100

    [UPDATE] form register and view register

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -22,11 +22,11 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $user = new User();
-        $user->setUsername($this->faker->userName);
+        $user->setUsername('wissal');
         $user->setFirstname($this->faker->firstName);
         $user->setLastname($this->faker->lastName);
         $user->setEmail($this->faker->email);
-        $user->setPhonenumber($this->faker->phoneNumber);
+        $user->setPhonenumber('123456789');
         $user->setRoles([]);
         $user->setToken($this->faker->randomNumber(2));
         $user->setZipcode($this->faker->postcode);
@@ -37,11 +37,11 @@ class UserFixtures extends Fixture
         $manager->persist($user);
 
         $admin = new User();
-        $admin->setUsername($this->faker->userName);
+        $admin->setUsername('nico');
         $admin->setFirstname($this->faker->firstName);
         $admin->setLastname($this->faker->lastName);
         $admin->setEmail($this->faker->email);
-        $admin->setPhonenumber($this->faker->phoneNumber);
+        $admin->setPhonenumber('0102030405');
         $admin->setRoles(['ROLE_ADMIN']);
         $admin->setToken($this->faker->randomNumber(2));
         $admin->setZipcode($this->faker->postcode);

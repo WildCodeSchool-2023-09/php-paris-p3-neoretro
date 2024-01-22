14faf26f39baccf38ab53636d395969f8bf0cc62
nicolasalibert <contact@nicolasalibert.me>
2024-01-22 17:02:35 +0100

    [ADDED] GamePlayed Controller

--- src/Controller/GamePlayedController.php
+++ src/Controller/GamePlayedController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\GamePlayed;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,7 +13,7 @@ class GamePlayedController extends AbstractController
     #[Route('/', name: 'index')]
     public function index(): Response
     {
-        $gamePlayed = null;
+        $gamePlayed = new GamePlayed();
 
         return $this->render('game_played/index.html.twig', [
             'gamePlayed' => $gamePlayed

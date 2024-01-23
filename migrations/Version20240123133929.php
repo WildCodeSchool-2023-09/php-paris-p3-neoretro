a581e01381f09aa7c9a7f8790f1a0faf496ab715
Baptiste Delefosse <baptiste.delefosse@hotmail.fr>
2024-01-23 15:28:13 +0100

    [UPDATE] carousel

--- migrations/Version20240123133929.php
+++ migrations/Version20240123133929.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240122152121 extends AbstractMigration
+final class Version20240123133929 extends AbstractMigration
 {
     public function getDescription(): string
     {

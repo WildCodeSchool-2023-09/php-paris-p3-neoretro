1958baf203491578358696fcd99bc67d01692d10
nicolasalibert <contact@nicolasalibert.me>
2024-01-26 16:56:01 +0100

    [UPDATED] Tables layout

--- assets/scripts/carouselGames.js
+++
@@ -1,7 +0,0 @@
-import { setVerticalCarousel } from "./carousel";
-
-const slideHeight = document.querySelector(".games").clientHeight + 50;
-const carousel = document.querySelector("#carousel");
-const sliderBtns = document.querySelectorAll('.slider .slider-btn');
-
-setVerticalCarousel(slideHeight, carousel, sliderBtns, 'current-slide', 380);

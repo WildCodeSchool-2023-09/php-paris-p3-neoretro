8464c367feec47868bace3005107a8e8d6db2e01
nicolasalibert <contact@nicolasalibert.me>
2024-02-06 10:57:18 +0100

    [UPDATED] New btn style and position

--- assets/scripts/carousel/carousel-big.js
+++
@@ -1,7 +0,0 @@
-import { setVerticalCarousel } from "./carousel-functions";
-
-const slideHeight = document.querySelector("li.slide").clientHeight + 50;
-const carousel = document.querySelector("ul.carousel");
-const sliderBtns = document.querySelectorAll('nav.slider .slider-btn');
-
-setVerticalCarousel(slideHeight, carousel, sliderBtns, 'current-slide', 500);
